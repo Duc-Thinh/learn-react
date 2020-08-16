ae728973da73dfd524cb2775c60f445bf8ee6449
Duc-Thinh <buiducthinh56@gmail.com>
2020-08-16 19:19:10 +0700

    new update

---
+++ demo-children/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Accordion from './components/accordion.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Accordion heading="Heading">
+        adadadknfkwnfkwanfkanf kwnfankfwakfn nakfnkwanf
+      </Accordion>
+    </div>
+  );
+}
+
+export default App;
